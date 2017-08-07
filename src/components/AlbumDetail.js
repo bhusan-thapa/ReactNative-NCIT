8daaa0239f754091afffd2b945f451dfaae2a7b2
se-fit <bhusan@bhusans-MacBook-Pro.local>
2017-08-07 22:31:56 +0545

    final

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Text, View, Image, Linking } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
+
+const AlbumDetail = ({ record }) => {
+  const { thumbnail_image, title, artist, image, url } = record;
+  const { headerContentStyle,
+     thumbnailStyle,
+     thumbnailContainerStyle,
+      headerTextStyle,
+      imageStyle } = styles;
+  return (
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image
+          source={{ uri: thumbnail_image }}
+          style={thumbnailStyle}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}> { title }</Text>
+          <Text> {artist} </Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+      <CardSection>
+        <Button text={'Buy Now'} onPress={() => Linking.openURL(url)} />
+      </CardSection>
+    </Card>
+  );
+};
+const styles = {
+  headerContentStyle: {
+    justifyContent: 'space-around',
+    flexDirection: 'column'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+    marginLeft: 10
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+};
+
+export default AlbumDetail;
